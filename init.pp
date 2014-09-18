2ee20ec05ddcb25bfe94ee556ac801e1489193a8
Andrew Roetker <andrew.roetker@puppetlabs.com>
2014-09-18 00:09:26 -0700

    Clean up the manifest for only the directory tree

--- init.pp
+++ init.pp
@@ -1,10 +1,11 @@
-$pe_testing = "/Users/aroetker/Projects/pe-testing"
-$scripts = "/Users/aroetker/Projects/pe-testing/scripts"
-$files = "/Users/aroetker/Projects/pe-testing/files"
-$tarballs = "/Users/aroetker/Projects/pe-testing/tarballs"
-$versions = "/Users/aroetker/Projects/pe-testing/versions"
-$packages = "/Users/aroetker/Projects/pe-testing/packages"
-$answers = "/Users/aroetker/Projects/pe-testing/answers"
+$projects = '/Users/aroetker/Projects'
+$pe_testing = "${projects}/pe-testing"
+$scripts    = "${projects}/pe-testing/scripts"
+$files      = "${projects}/pe-testing/files"
+$tarballs   = "${projects}/pe-testing/tarballs"
+$versions   = "${projects}/pe-testing/versions"
+$packages   = "${projects}/pe-testing/packages"
+$answers    = "${projects}/pe-testing/answers"
 file { [ $pe_testing,
          $scripts,
          $files,
@@ -13,8 +14,3 @@ file { [ $pe_testing,
          $answers, ] :
            ensure => directory,
 }
-
-$gemfile_content = "source 'https://rubygems.org'
-gem 'puppet', '~> 3.7.1'"
-$bashrc_conent = "alias installpe='/vagrant/puppet-enterprise/puppet-enterprise-uninstaller -dpy -l /vagrant/uninstall.log && /vagrant/puppet-enterprise/puppet-enterprise-installer -D -a /vagrant/answers/#{name}.answer -l /vagrant/#{name}-install.log'
-PS1='\\[\\e[0;31m\\]\\u\\[\\e[m\\] \\[\\e[1;34m\\]\\w\\[\\e[m\\] \\[\\e[0;31m\\]\\$ \\[\\e[m\\]\\[\\e[0;32m\\]'"
